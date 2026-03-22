5a82e020946edb3a6dc1238e9031271062a5785c
Nestwork <post@nestwork.no>
2026-03-22 01:16:15 +0000

    Add CORS support for Capacitor iOS app

--- server/index.ts
+++ server/index.ts
@@ -27,6 +27,26 @@ declare module "http" {
   }
 }
 
+const allowedOrigins = [
+  "capacitor://localhost",
+  "http://localhost",
+  "http://localhost:5000",
+];
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (origin && allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  }
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 app.use(
   express.json({
     verify: (req, _res, buf) => {
